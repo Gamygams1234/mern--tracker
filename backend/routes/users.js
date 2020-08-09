07b881e17d1818a5229ed68cf48ea7b2cfb5efb4
Gamygams1234 <gamyburgos@gmail.com>
2020-08-09 13:31:21 -0700

    Adding excersize and users routes

---
+++ backend/routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+let User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  // this will find all of the users
+  User.find()
+    // return it in json
+    .then((users) => res.json(users))
+    // errors will have a 400 status
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+
+  const newUser = new User({ username });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
